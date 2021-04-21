73e4d017c9db1a9e5feba8278ef786f705a48ae9
pablopenrose <kaderarnold@icloud.com>
2021-04-21 03:52:56 -0400

    used the useHistory react hook to toggle state change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,13 +8,13 @@ class Footer extends Component {
 			<footer>
 				<address>
 					<a style={{marginRight: "0.5rem"}} href="https://www.instagram.com/a_social_engineer/"
-						className="fa fa-instagram footer-icon" target= "_blank"></a>	
+						className="fa fa-instagram footer-icon" rel="noreferrer" target= "_blank"></a>	
 					<a style={{marginRight: "0.5rem"}} href="https://twitter.com/nosocialengine"
-						className="fa fa-twitter footer-icon" target="_blank"></a>
+						className="fa fa-twitter footer-icon" rel="noreferrer" target="_blank"></a>
 					<a style={{marginRight: "0.5rem"}} href="https://www.github.com/pablopenrose/" 
-						className="fa fa-github footer-icon" target="_blank"></a>
+						className="fa fa-github footer-icon" rel="noreferrer" target="_blank"></a>
 					<a style={{marginRight: "0.5rem"}} href="https://www.linkedin.com/in/kader-arnold-77008a210" 
-						className="fa fa-linkedin footer-icon" target="_blank"></a>
+						className="fa fa-linkedin footer-icon" rel="noreferrer" target="_blank"></a>
 					<a style={{fontSize: "90%", opacity: "80%",  float: "right", marginRight: "1.25rem", marginTop: "2px"}} href="mailto:kaderarnold@icloud.com">inquiries</a>
 				</address>
 			</footer>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,37 +1,54 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router';
 import {ButtonGroup, Button} from 'react-bootstrap';
 
 import '../App.js'
-import Bio from './Bio';
 
-class Nav extends Component {
+function Nav () {
+
+	let history = useHistory ()
+	const [on, toggle] = useState(0);
+
+	useEffect (() => {
+		if (on == 1) {
+			document.getElementById("bio").innerHTML = "back";
+
+		}
+		else {
+			document.getElementById("bio").innerHTML = "bio";
+			history.push ("");
+		}
+	});
 	
-	render () {
-		const style = {
-			background: "#ffda669", 
-			margin: "0px",
-			padding: "0px", 
-			display: "inline-block",
-			width: "98%"
-		};
-		const buttonStyle = {
-			margin: "0.5rem 0rem 0.5rem 0rem", 
-			float: "right" 
-		};
-		return (
-		  <nav style={style}>
-			  <Link style={{color: 'black', display: "inline-block"}} to="/" >
-				  <h1 style={{margin: "0.7rem 0.5rem 0.5rem 0rem"}}>Kader Arnold</h1>
-			  </Link>
-			 <ButtonGroup style={buttonStyle} toggle>
-				  <Link to="/bio" style={{margin: "0.5rem 0rem 0.5rem 0.5rem"}}>
-					  <Button id="bio" style={{marginLeft: "1rem"}}>bio</Button>
-				  </Link>
-			</ButtonGroup>
-		  </nav>
-		);
+	function back () {
+				
+	}
+
+	const style = {
+		background: "#ffda669", 
+		margin: "0px",
+		padding: "0px", 
+		display: "inline-block",
+		width: "98%"
 	};
+	const buttonStyle = {
+		margin: "0.5rem 0rem 0.5rem 0rem", 
+		float: "right" 
+	};
+
+	return (
+	  <nav style={style}>
+		  <Link style={{color: 'black', display: "inline-block"}} to="/" >
+			  <h1 style={{margin: "0.7rem 0.5rem 0.5rem 0rem"}}>Kader Arnold</h1>
+		  </Link>
+		 <ButtonGroup style={buttonStyle} toggle>
+			  <Link to="/bio" style={{margin: "0.5rem 0rem 0.5rem 0.5rem"}}>
+				  <Button onClick={() => toggle(on == 0 ? 1 : 0)} id="bio" style={{marginLeft: "1rem"}}>bio</Button>
+			  </Link>
+		</ButtonGroup>
+	  </nav>
+	);
 
 };
 
