07c0758773ff9addc1acbdd6f81bfdfa79136157
pablopenrose <kaderarnold@icloud.com>
2021-04-21 05:40:00 -0400

    updated Bio.js content

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -13,7 +13,7 @@ function Bio () {
 							<h5>Full Stack Development</h5>
 						</Card.Title>
 						<Card.Text style={{padding: "0.5rem", margin: "0rem 0.5rem"}}>
-							<p> I practice full stack web development using either a LAMP stack, or a full stack javascript infrastructure. For front-end development, I perfer React to other frameworks (Vue, Angular) because of its speed. In tandem with the single threaded nature of nodejs, this js-stack approach is inceredibly fast and good for seo. 
+							<p> I practice full stack web development using either a LAMP stack, or a PERN stack. For front end development, I perfer React to other frameworks (Vue, Angular) because of its speed and modularity.  
 							</p>
 						</Card.Text>
 					</Card>
@@ -23,16 +23,16 @@ function Bio () {
 							<h5>Content Writing</h5>
 						</Card.Title>
 						<Card.Text style={{padding: "0.5rem", margin: "0rem 0.5rem"}}>
-							<p>I often write white papers or technical analysis when bloggers or companies are in a position where they don't have t    he time to curate a fully seo-optimized 1500-2000 word article. I like to blog in the cryptocurrency sphere, and am confident in technical writing with r    egards to information technology.</p>
+							<p>I often write white papers or technical analysis when bloggers or companies are in a position where they don't have the time to write (and edit) a fully optimized 1500-2000 word article.</p>
 						</Card.Text>
 					</Card>
 					<hr />
 					<Card bg="info">
 						<Card.Title style={{padding: "0.5rem", margin: "0.5rem"}}>
-							<h5>UX/UI Development</h5>
+							<h5>Game and GUI Programming</h5>
 						</Card.Title>
 						<Card.Text style={{padding: "0.5rem", margin: "0rem 0.5rem"}}>
-							<p>In the past I used the tkinter module in python to implement GUI's, and generally speaking this is what i believe to b    e an excellent quick and dirty solution. That being said, we can also use the HTML/CSS/JS suite through the nodejs module electron, for a more profession    al feel. Electron is also platform independent. The catch being, electron was build on the chromium engine.</p>
+							<p>In the past I used the tkinter module to implement GUI's. This is a python3 api for a library called tcl. I also like electron, which is a node module for writing desktop apps using html. I like to write games using the pyopengl and pygame modules.</p>
 						</Card.Text>
 					</Card>
 					<hr />
